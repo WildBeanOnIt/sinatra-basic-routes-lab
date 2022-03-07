8769466c37a4b00bf80b75f2eeb6bf1f80c8e490
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-03-06 19:04:07 -0600

    commit tree 227636248ff7d290ab0328203ea9c350d656ebe2

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+    get '/name' do
+        "My name is Julio"
+    end
+
+    get '/hometown' do
+        "My hometown is Roud Rock, Tx"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Lotta cake"
+    end
 end
